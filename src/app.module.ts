94097ad67b8268003762b3e03c8392570c243e40
yuikoito <yuiko.dev@gmail.com>
2023-03-21 23:54:36 +0100

    chore: add bodyParser.urlencoded

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import * as bodyParser from 'body-parser';
 
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(bodyParser.urlencoded({ extended: true })).forRoutes('*');
+  }
+}
